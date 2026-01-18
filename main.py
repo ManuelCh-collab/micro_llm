091a3b8faeb173d7a39e193a1d5c966eae74c6e7
endersonsaimonvillalba-ai <endersonsaimonvillalba@.com>
2026-01-18 18:17:48 -0400

    implementando clase Config en entry point

--- main.py
+++ main.py
@@ -1,24 +1,23 @@
-from app.embedding_logic import TransformerEmbedding
-from app.tokenizer_logic import BPETokenizer
 import torch
+from app.config import Config
+from app.tokenizer_logic import BPETokenizer
+from app.embedding_logic import TransformerEmbedding
 
-VOCAB_SIZE = 5000
-D_MODEL = 256
-MAX_SEQ_LEN = 512
-
-tokenizer = BPETokenizer(vocab_size=VOCAB_SIZE)
+tokenizer = BPETokenizer(vocab_size=Config.VOCAB_SIZE)
 
 tokenizer.train(["entrenamiento.txt"])
 tokenizer.save("kracker/kracker2.json")
-
-#tokenizer.load("kracker/kracker2.json")
 
 input_text = "Aprender Python es genial"
 tokens_ids = tokenizer.encode(input_text)
 print(f"Texto: {input_text}")
 print(f"Tokens IDs: {tokens_ids}")
 
-embedder = TransformerEmbedding(vocab_size=VOCAB_SIZE, d_model=D_MODEL,max_seq_len=MAX_SEQ_LEN)
+embedder = TransformerEmbedding(
+    vocab_size=Config.VOCAB_SIZE,
+    d_model=Config.D_MODEL,
+    max_seq_len=Config.MAX_SEQ_LEN
+    )
 
 input_tensor = torch.tensor(tokens_ids).unsqueeze(0)
 
